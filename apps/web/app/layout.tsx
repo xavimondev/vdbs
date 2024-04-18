143add2dd64d9e64a935fc11fb6ac2ba727cf2ae
Xavi Alfaro <nxb.developer@gmail.com>
2024-04-18 13:00:41 -0500

    add tailwind and shadcn

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,7 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { cn } from "../lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,7 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, "dark")}>{children}</body>
     </html>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page(): JSX.Element {
   return (
     <main>
-      <h1>Supamigration</h1>
+      <h1 className="text-4xl text-red-200">Supamigration</h1>
     </main>
   );
 }
