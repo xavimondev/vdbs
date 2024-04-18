9ad048bb065f0e8a9d1a48c3c57c5e6ac1f68223
Xavi Alfaro <nxb.developer@gmail.com>
2024-04-18 18:12:53 -0500

    add toast ui component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         )}
       >
         <main className="grow flex flex-col lg:flex-row gap-6">{children}</main>
+        <Toaster />
       </body>
     </html>
   );
