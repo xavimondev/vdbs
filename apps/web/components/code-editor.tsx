0c96bbdd14a017c769b8ebb6475a2df817d2337a
Xavi Alfaro <nxb.developer@gmail.com>
2024-04-18 20:51:48 -0500

    update ui code editor

--- apps/web/components/code-editor.tsx
+++ apps/web/components/code-editor.tsx
@@ -1,10 +1,8 @@
 "use client";
 import { useEffect } from "react";
 import dynamic from "next/dynamic";
-import type monacoTypes from "monaco-editor";
 import { useMonaco } from "@monaco-editor/react";
 import { LoaderIcon } from "lucide-react";
-import theme from "@/components/editor-themes/odp.json";
 
 const Monaco = dynamic(() => import("@monaco-editor/react"), { ssr: false });
 
@@ -16,20 +14,76 @@ export function CodeEditor({ code }: CodeEditorProps) {
   const monaco = useMonaco();
 
   useEffect(() => {
-    if (monaco) {
-      monaco.editor.defineTheme(
-        "odp",
-        theme as monacoTypes.editor.IStandaloneThemeData
-      );
-      monaco.editor.setTheme("odp");
-      // https://github.com/brijeshb42/monaco-themes/tree/master/src
-    }
+    if (!monaco) return;
+    // https://github.com/brijeshb42/monaco-themes/tree/master/src
+    monaco.editor.defineTheme("vs-dark", {
+      base: "vs-dark",
+      inherit: true,
+      rules: [
+        {
+          background: "141414",
+          token: "",
+        },
+        {
+          foreground: "5f5a60",
+          fontStyle: "italic",
+          token: "comment",
+        },
+        {
+          foreground: "cf6a4c",
+          token: "constant",
+        },
+        {
+          foreground: "9b703f",
+          token: "entity",
+        },
+        {
+          foreground: "cda869",
+          token: "keyword",
+        },
+        {
+          foreground: "f9ee98",
+          token: "storage",
+        },
+        {
+          foreground: "8f9d6a",
+          token: "string",
+        },
+        {
+          foreground: "9b859d",
+          token: "support",
+        },
+        {
+          foreground: "7587a6",
+          token: "variable",
+        },
+        {
+          foreground: "daefa3",
+          token: "string source",
+        },
+        {
+          foreground: "ddf2a4",
+          token: "string constant",
+        },
+        {
+          foreground: "e9c062",
+          token: "string.regexp",
+        },
+      ],
+      colors: {
+        "editor.foreground": "#F8F8F8",
+        "editor.background": "#111010",
+        "editor.selectionBackground": "#DDF0FF33",
+        "editor.lineHighlightBackground": "#FFFFFF08",
+        "editorCursor.foreground": "#A7A7A7",
+        "editorWhitespace.foreground": "#FFFFFF40",
+      },
+    });
   }, [monaco]);
 
   useEffect(() => {
-    if (monaco) {
-      monaco.editor.getModels()[0]?.setValue(code || "");
-    }
+    if (!monaco) return;
+    monaco.editor.getModels()[0]?.setValue(code || "");
   }, [code]);
 
   return (
@@ -39,18 +93,19 @@ export function CodeEditor({ code }: CodeEditorProps) {
       value={code}
       className="h-[91vh]"
       loading={
-        <div className="flex h w-screen items-center justify-center">
+        <div className="flex w-1/2 items-center justify-center">
           <LoaderIcon />
         </div>
       }
       options={{
-        readOnly: true,
+        readOnly: false,
         padding: {
           top: 20,
         },
         cursorSmoothCaretAnimation: "on",
+        language: "sql",
         cursorBlinking: "smooth",
-        fontSize: 17,
+        fontSize: 16,
         formatOnType: true,
         formatOnPaste: true,
         automaticLayout: true,
