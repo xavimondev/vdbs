3ba6d69cc71513460d11defd937f6e320a28b04e
Xavi Alfaro <nxb.developer@gmail.com>
2024-04-19 16:45:40 -0500

    add initial command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,23 @@
-console.log("cli");
+#!/usr/bin/env node
+import { Command } from "commander";
+import { getPackageInfo } from "@/utils/package-info";
+
+process.on("SIGINT", () => process.exit(0));
+process.on("SIGTERM", () => process.exit(0));
+
+async function main() {
+  const packageInfo = getPackageInfo();
+
+  const program = new Command()
+    .name("vdbs")
+    .description("Add a migration schema to your Supabase project")
+    .version(
+      packageInfo.version || "0.0.1",
+      "-v, --version",
+      "display the version number"
+    );
+
+  program.parse();
+}
+
+main();
---
+++ packages/cli/src/utils/package-info.ts
@@ -0,0 +1,8 @@
+import path from "node:path";
+import fs from "node:fs";
+import { type PackageJson } from "type-fest";
+
+export const getPackageInfo = (): any => {
+  const packageJsonPath = path.join("package.json");
+  return JSON.parse(fs.readFileSync(packageJsonPath, "utf-8")) as PackageJson;
+};
