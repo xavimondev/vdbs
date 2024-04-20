93fa2c026fafed9548be2cc67290fda15f7499bf
Xavi Alfaro <nxb.developer@gmail.com>
2024-04-20 15:11:23 -0500

    remove fetch dependency

---
+++ packages/cli/CHANGELOG.md
@@ -0,0 +1,7 @@
+# vdbs
+
+## 0.0.2
+
+### Patch Changes
+
+- update fetch
--- packages/cli/src/utils/get-schema.ts
+++ packages/cli/src/utils/get-schema.ts
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
 const ENDPOINT = `https://vdbs.vercel.app/api/get-generation?code=`;
 
