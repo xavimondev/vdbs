e441ed43cce9c070c69b7222157e6b71a12fc53e
Xavi Alfaro <nxb.developer@gmail.com>
2024-04-19 22:14:40 -0500

    add metadata seo

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,14 +2,37 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { APP_URL } from "@/constants";
 import { Toaster } from "@/components/ui/sonner";
 import { Header } from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "vdbs - Transform Designs to Database Schemas with AI";
+const description =
+  "Manage database migrations with a single click, accessing AI-generated SQL code from a database diagram and a command line script for instant local execution.";
+
 export const metadata: Metadata = {
-  title: "supamigration",
-  description: "Supamigration",
+  metadataBase: new URL(APP_URL),
+  title,
+  description,
+  keywords: ["vision ai", "supabase", "postgress", "sql", "migrations"],
+  openGraph: {
+    title,
+    description,
+    url: "/",
+    siteName: "vdbs",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "/banner.png",
+        width: 1835,
+        height: 1000,
+        type: "image/jpeg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
