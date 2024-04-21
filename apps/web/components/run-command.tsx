9a6a372ed0b13785ea85b37ebd7d7b06340d1903
Xavi Alfaro <nxb.developer@gmail.com>
2024-04-21 11:22:12 -0500

    update copy clipboard

--- apps/web/components/run-command.tsx
+++ apps/web/components/run-command.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { Check, Terminal } from "lucide-react";
-import { useClipboard } from "@/hooks/use-clipboard";
+import { Check, Clipboard } from "lucide-react";
 import { copyToClipboard } from "@/utils";
 import { useSchemaStore } from "@/store";
+import { useClipboard } from "@/hooks/use-clipboard";
 import { CodeCommand } from "@/components/code-command";
 
 export function RunCommand() {
@@ -11,48 +11,32 @@ export function RunCommand() {
   const { cmdCode } = schema ?? {};
 
   return (
-    <button
-      className="shrink-0 
-      justify-center 
-      text-sm 
-      font-medium 
-      transition-colors 
-      hover:bg-white/90 
-      dark:hover:bg-white/5 
-      h-8 
-      flex 
-      items-center 
-      text-zinc-600
-      dark:text-zinc-50 
-      shadow-none 
-      bg-white
-      dark:bg-neutral-800 
-      w-full 
-      px-3 
-      py-1.5 
-      rounded-lg 
-      border 
-      border-zinc-700"
-      onClick={async () => {
-        if (cmdCode && cmdCode !== "") {
-          setIsCopied(!isCopied);
-          await copyToClipboard(`npx vdbs add ${cmdCode}`);
-        }
-      }}
+    <div
+      className="shrink-0 justify-center text-sm font-medium transition-colors h-8 flex items-center text-zinc-600 
+      dark:text-zinc-50 shadow-none bg-white dark:bg-neutral-800 w-full px-3 py-1.5 rounded-md border border-zinc-700"
     >
-      <div className="flex items-center flex-1 gap-2 font-mono text-xs sm:text-sm">
-        {isCopied ? (
-          <Check className="w-4 h-4" />
-        ) : (
-          <Terminal className="w-4 h-4" />
-        )}
+      <div className="flex items-center flex-1 gap-2 font-mono text-xs sm:text-sm relative">
         {cmdCode && cmdCode !== "" && (
           <>
             npx vdbs add <CodeCommand commandCode={cmdCode} />
           </>
         )}
+        <button
+          className="absolute right-0"
+          onClick={async () => {
+            if (cmdCode && cmdCode !== "") {
+              setIsCopied(!isCopied);
+              await copyToClipboard(`npx vdbs add s929dkd`);
+            }
+          }}
+        >
+          {isCopied ? (
+            <Check className="size-4" />
+          ) : (
+            <Clipboard className="size-4" />
+          )}
+        </button>
       </div>
-      <span className="sr-only">Copy cli command</span>
-    </button>
+    </div>
   );
 }
