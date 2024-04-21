fcf85988766c01f2b6691cef307c3bc1355f075e
Xavi Alfaro <nxb.developer@gmail.com>
2024-04-21 11:29:38 -0500

    add ui enhacements

--- apps/web/components/deploy-result.tsx
+++ apps/web/components/deploy-result.tsx
@@ -10,32 +10,25 @@ export function DeployResult({ supabaseLinkTables }: DeployResultProps) {
   return (
     <div className="size-full flex flex-col gap-2 w-full animate-fade-slide">
       <p className="text-muted-foreground text-sm">
-        View the results by clicking on the link:
+        View the results on the following link:
       </p>
       <div className="flex items-center gap-3">
-        <button
-          className="justify-center text-sm font-medium transition-colors hover:bg-white/90 dark:hover:bg-white/5 h-8 flex items-center 
-      text-zinc-600
-      dark:text-zinc-50 
-      shadow-none 
-      bg-white
-      dark:bg-neutral-800 
-      w-full 
-      px-3 
-      py-1.5 
-      rounded-lg 
-      border 
-      border-zinc-700"
+        <div
+          className="justify-center text-sm font-medium transition-colors h-8 flex text-zinc-600 dark:text-green-200 bg-white 
+          dark:bg-neutral-800 w-full px-3 py-1.5 rounded-md border border-zinc-700"
         >
-          <div className="flex items-center flex-1 gap-2 ">
-            <span className="text-xs sm:text-sm text-green-500 dark:text-green-200">
-              {supabaseLinkTables}
-            </span>
+          <div className="flex items-center flex-1 relative">
+            <span className="text-xs sm:text-sm">{supabaseLinkTables}</span>
+            <a
+              href={supabaseLinkTables}
+              target="_blank"
+              rel="noreferrer"
+              className="absolute right-0"
+            >
+              <ExternalLink className="size-4" />
+            </a>
           </div>
-        </button>
-        <a href={supabaseLinkTables} target="_blank">
-          <ExternalLink />
-        </a>
+        </div>
       </div>
     </div>
   );
