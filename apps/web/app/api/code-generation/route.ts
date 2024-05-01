5f28f422e489ef423abe2bde102aa358ca14e71b
Xavi Alfaro <nxb.developer@gmail.com>
2024-05-01 17:55:29 -0500

    remove rate limit since apikey is required

--- apps/web/app/api/code-generation/route.ts
+++ apps/web/app/api/code-generation/route.ts
@@ -2,29 +2,44 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
-import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
-import { TOTAL_GENERATIONS } from "@/constants";
+// import { Ratelimit } from "@upstash/ratelimit";
+// import { Redis } from "@upstash/redis";
+// import { TOTAL_GENERATIONS } from "@/constants";
 import { PROMPT } from "@/prompt";
 
 const openai = new OpenAI();
 
 export const runtime = "edge";
 
-const ratelimit =
-  process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN
-    ? new Ratelimit({
-        redis: new Redis({
-          url: process.env.UPSTASH_REDIS_REST_URL,
-          token: process.env.UPSTASH_REDIS_REST_TOKEN,
-        }),
-        limiter: Ratelimit.slidingWindow(TOTAL_GENERATIONS, "1440 m"), // 1 per day
-        analytics: true,
-      })
-    : false;
+// const ratelimit =
+//   process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN
+//     ? new Ratelimit({
+//         redis: new Redis({
+//           url: process.env.UPSTASH_REDIS_REST_URL,
+//           token: process.env.UPSTASH_REDIS_REST_TOKEN,
+//         }),
+//         limiter: Ratelimit.slidingWindow(TOTAL_GENERATIONS, "1440 m"), // 1 per day
+//         analytics: true,
+//       })
+//     : false;
 
 export async function POST(req: Request) {
-  if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
+  const customApiKey = cookies().get("api-key")?.value;
+
+  if (process.env.NODE_ENV === "production" && !customApiKey) {
+    return NextResponse.json(
+      {
+        data: undefined,
+        error: "Missing API KEY – make sure to set it.",
+      },
+      { status: 400 }
+    );
+  }
+
+  if (
+    process.env.NODE_ENV === "development" &&
+    (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "")
+  ) {
     return NextResponse.json(
       {
         data: undefined,
@@ -35,34 +50,31 @@ export async function POST(req: Request) {
     );
   }
 
-  const customApiKey = cookies().get("api-key")?.value;
-  const hasCustomApiKey = customApiKey && customApiKey.trim().length > 0;
-
-  if (process.env.NODE_ENV === "production" && !customApiKey) {
-    if (ratelimit) {
-      const ip = req.headers.get("x-real-ip") ?? "local";
-
-      const { success, limit, reset, remaining } = await ratelimit.limit(ip);
-      if (!success) {
-        return NextResponse.json(
-          { message: "You have reached your request limit for the day." },
-          {
-            status: 429,
-            headers: {
-              "X-RateLimit-Limit": limit.toString(),
-              "X-RateLimit-Remaining": remaining.toString(),
-              "X-RateLimit-Reset": reset.toString(),
-            },
-          }
-        );
-      }
-    }
-  }
-
-  if (hasCustomApiKey) {
+  if (customApiKey) {
     // Set user's api key
-    openai.apiKey = customApiKey;
+    openai.apiKey = customApiKey as string;
   }
+
+  // const hasCustomApiKey = customApiKey && customApiKey.trim().length > 0;
+
+  // if (ratelimit) {
+  //   const ip = req.headers.get("x-real-ip") ?? "local";
+
+  //   const { success, limit, reset, remaining } = await ratelimit.limit(ip);
+  //   if (!success) {
+  //     return NextResponse.json(
+  //       { message: "You have reached your request limit for the day." },
+  //       {
+  //         status: 429,
+  //         headers: {
+  //           "X-RateLimit-Limit": limit.toString(),
+  //           "X-RateLimit-Remaining": remaining.toString(),
+  //           "X-RateLimit-Reset": reset.toString(),
+  //         },
+  //       }
+  //     );
+  //   }
+  // }
 
   const { prompt: base64 } = await req.json();
 
