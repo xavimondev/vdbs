169cc656a87152cb55300c1d47a2ec407f5746d4
Xavi Alfaro <nxb.developer@gmail.com>
2025-04-20 23:23:54 -0500

    update footer component to include text size for better readability

--- apps/web/components/footer.tsx
+++ apps/web/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className='pt-4 flex justify-center dark:text-green-200 text-green-500 w-full'>
-      <div className='flex gap-2 items-center font-medium'>
+      <div className='flex gap-2 items-center font-medium text-sm'>
         <p>Made by </p>
         <a href='https://twitter.com/xavimonp' aria-label='My twitter account' target='_blank'>
           xavimondev
