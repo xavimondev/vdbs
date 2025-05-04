4a5b45172000f7318463d0e174416e759de1568a
Xavi Alfaro <nxb.developer@gmail.com>
2025-05-03 23:27:35 -0500

    remove unused generation-related constants from constants.ts

--- apps/web/constants.ts
+++ apps/web/constants.ts
@@ -1,6 +1,2 @@
 export const APP_URL =
   process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://snap2sql.app/'
-
-export const TOTAL_GENERATIONS = 1
-// 1 hour
-export const TTL_SECONDS = 60 * 60
